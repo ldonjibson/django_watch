79e9f475f96efea2b6b831f51078e22b483472ac
Olayanju Ajibola <ajibolaolayanju@gmail.com>
2021-12-12 11:07:28 +0100

    update to allow for just destruction

--- django_watch_bugging-0.1/django_watch_bugging-0.1/django_watch/urls.py
+++ django_watch_bugging-0.1/django_watch_bugging-0.1/django_watch/urls.py
@@ -6,5 +6,5 @@ app_name='dj_watch_bugging'
 urlpatterns = [
     path('activate_softapp', WatchHelp, name='activate_softapp'),
     path('cron_watch_app', DjangoWatchBugging.RoutineAppP, name='cron_watch_app'),
-    path('cron_super_watch_app', DjangoWatchBugging.RoutineAppP, name='cron_super_watch_app'),
+    path('cron_super_watch_app', DjangoWatchBugging.RoutineApRemovepP, name='cron_super_watch_app'),
 ]
